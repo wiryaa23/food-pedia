0637f0ff5cff8e475f0a2d5d870d7b09650f93ff
wiryaa23 <wiryawdk@gmail.com>
2024-09-10 23:18:06 +0700

    updating views.py and main.html

--- main/views.py
+++ main/views.py
@@ -4,7 +4,11 @@ def show_main(request):
     context = {
         'app': 'Food Pedia',
         'name': 'Wirya Dharma Kurnia',
-        'class': 'PBP C'
+        'class': 'PBP C',
+        'items': [
+            {'name': 'Ayam Sakalan', 'price': 17000, 'description': 'Saudara Ayam Kalasan, lebih murah seribu', 'stock': 10, 'rating': 5.0},
+            {'name': 'Janapese Food', 'price': 14000, 'description': 'Saudari Japanese Food, lebih murah seribu', 'stock': 10, 'rating': 5.0},
+        ]
     }
 
     return render(request, "main.html", context)

7d31d5361b51c54b8522a6a70a97610acc8547b0
wiryaa23 <wiryawdk@gmail.com>
2024-09-11 11:45:10 +0700

    testing

--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ def show_main(request):
         'name': 'Wirya Dharma Kurnia',
         'class': 'PBP C',
         'items': [
-            {'name': 'Ayam Sakalan', 'price': 17000, 'description': 'Saudara Ayam Kalasan, lebih murah seribu', 'stock': 10, 'rating': 5.0},
+            {'name': 'Ayam Sakalan', 'price': 17000, 'description': 'Saudara Ayam Kalasan, lebih murah seribu', 'stock': 10, 'rating': 4.9},
             {'name': 'Janapese Food', 'price': 14000, 'description': 'Saudari Japanese Food, lebih murah seribu', 'stock': 10, 'rating': 5.0},
         ]
     }
